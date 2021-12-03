4df6897fdd439148bf7f72d790931fa7d47660d7
Asher Bay <asherbay@gmail.com>
2021-12-03 15:40:54 -0700

    setup

---
+++ db/seeds.rb
@@ -0,0 +1,36 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+
+Category.destroy_all
+camping = Category.create(name: "Camping")
+fishing = Category.create(name: "Fishing")
+skiing = Category.create(name: "Skiing")
+climbing = Category.create(name: "Climbing")
+biking = Category.create(name: "Biking")
+running = Category.create(name: "Running")
+hiking = Category.create(name: "Hiking")
+utv = Category.create(name: "UTV")
+kayaking = Category.create(name: "Kayaking")
+clothing = Category.create(name: "Clothing")
+
+
+camping.items.create(name: , price: , description: , condition: )
+camping.items.create(name: , price: , description: , condition: )
+camping.items.create(name: , price: , description: , condition: )
+camping.items.create(name: , price: , description: , condition: )
+camping.items.create(name: , price: , description: , condition: )
+camping.items.create(name: , price: , description: , condition: )
+camping.items.create(name: , price: , description: , condition: )
+camping.items.create(name: , price: , description: , condition: )
+camping.items.create(name: , price: , description: , condition: )
+camping.items.create(name: , price: , description: , condition: )
+
+
