a7120965969067ed98edc03fe6fdd3d59744341c
Asher Bay <asherbay@gmail.com>
2021-12-04 10:13:37 -0700

    started items component

--- client/src/components/Items.js
+++ client/src/components/Items.js
@@ -1,7 +1,33 @@
-const Items = () => {
+import {useState, useEffect} from "react"
+import {useLocation} from "react-router"
+import axios from 'axios'
+
+const Items = (props) => {
+    const [items, setItems] = useState([])
+    const location = useLocation()
+    const category = location.state
+
+    useEffect(()=>{
+        getItems()
+    } , [])
+
+    const getItems = async () => {
+        try { let res = await axios.get(`/api/categories/${category.id}/items`)
+        setItems(res.data)
+        } catch (err){
+            console.log("error: " + err.data)
+        } 
+    }
+
+    const renderItems = () => {
+        items.map((item) => <Item key={item.id} {...item}/>)
+    }
+
+
     return (
         <div>
             <h1>Items</h1>
+            <p>{renderItems()}</p>
         </div>
 
     )
