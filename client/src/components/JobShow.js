d3955c4491044a9e3f01f032d8f2018a8a051e18
John Leosco <jrleosco@gmail.com>
2021-12-04 13:41:53 -0700

    JobShow/Jobs completed

--- client/src/components/JobShow.js
+++ client/src/components/JobShow.js
@@ -1,9 +1,42 @@
-const JobShow = () => {
-    return (
-        <div>
-            <h1>JobShow</h1>
-        </div>
+import axios from 'axios';
+import React, {useState, useEffect} from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import JobForm from "./JobForm"
 
-    )
-}
+const JobShow = () => {
+    const [job, setJob] = useState("");
+    const navigate = useNavigate();
+    const params = useParams();
+    const [showEdit, setShowEdit] = useState(false);
+  
+    useEffect(() => {
+      getJob();
+    }, []);
+  
+    const getJob = async () => {
+      try {
+        let data = await axios.get(`/api/jobs/${params.id}`);
+        setJob(data.data);
+      } catch (err) {
+        alert("Error Occurred Getting Job");
+      }
+    };
+    const deleteJob = async () => {
+      await axios.delete(`/api/jobs/${params.id}`);
+      navigate("/jobs");
+    };
+    return(
+      <div>
+         <h3> {job.title}</h3>
+         <p>At {job.company}</p>
+         <p>Annual Salary: ${job.salary}</p>
+         <button onClick={() => setShowEdit(!showEdit)}>
+            {showEdit ? "Hide" : "Update"}
+            </button>
+         <button onClick={()=>deleteJob()} >Delete</button>
+         {showEdit && <JobForm />}
+      </div>
+    );
+  }
+
 export default JobShow;
