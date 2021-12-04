a1d4f1979f5bda902cf9e074ab63be7115eee6cf
Madeline Armstrong <armstrong.madeline@gmail.com>
2021-12-04 13:34:48 -0700

    more css on home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import tent from './tent.svg'
 
 const Home = () => {
     return (
@@ -9,13 +10,17 @@ const Home = () => {
                 <h2 className='front-page-h2'>Your home away from home for all things outdoors!</h2>
             </div>
             <div className='front_categories'>
-                <img source='https://www.clipartmax.com/png/middle/220-2209526_free-png-black-tent-png-images-transparent-camp-icon-png.png' alt='tent icon'/>
+                <img src={tent} style={{width: '100px' }} alt='tent icon'/>
                 <h3 className='front-page-h3'>Gear for Sale</h3>
                 <Link className='button-24' to={`/categories/`}>Explore Gear</Link>
             </div>
             <div className='work-img'>
                 <h3 className='front-page-h3'>Work in the Outdoors</h3>
                 <Link className='button-24' to={`/jobs/`}>View Jobs</Link>
+            </div>
+            <div className='front_categories'>
+                <h3 className='front-page-h3'>Our friends in the outdoors share gear tips for your next adventure.</h3>
+                <Link className='button-24' to={`/articles/`}>See What's New</Link>
             </div>
         </div>
 
