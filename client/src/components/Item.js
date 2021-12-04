a26523e5f928023937885e982b4badfb6d30a8cb
Asher Bay <asherbay@gmail.com>
2021-12-04 15:59:35 -0700

    another merge res

--- client/src/components/Item.js
+++ client/src/components/Item.js
@@ -8,18 +8,10 @@ const Item = (props) => {
             <h1>{name}</h1>
             <p>${price}</p>
             <p>{description}</p>
-<<<<<<< HEAD
-            <p>condition: {condition}</p>
+            <p>Condition: {condition}</p>
             <Link to={`/categories/${category_id}/items/${id}`}>view</Link>
 
             <button onClick={() => deleteItem(id)}>delete</button>
-=======
-            <p>Condition: {condition}</p>
-            <div>
-            <Link className='button-25' to={`/bugs/${category_id}/items/${id}/edit`}>edit</Link>
-            <button className='button-25' onClick={() => deleteItem(id)}>delete</button>
-            </div>
->>>>>>> 9bf6d4c2607e025b9070b20c47cc5f359c562d02
         </div>
 
     )
